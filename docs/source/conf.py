9211b2b972ee3ae04760e1c133b8b30313b09ed9
Jordan Henshaw <162933818+jordanhenshaw@users.noreply.github.com>
2024-03-17 23:01:24 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Sorcerer'
+copyright = '2024, Alva Theaters'
+author = 'Alva Theaters'
 
 release = '0.1'
 version = '0.1.0'
